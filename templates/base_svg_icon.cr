73cff2a89fc56423c3ea6b4f9403e682ca181e1f
Wout <hi@wout.codes>
2021-08-06 16:58:01 +0100

    fix: update link to documentation on repo in templates

--- templates/base_svg_icon.cr
+++ templates/base_svg_icon.cr
@@ -1,5 +1,5 @@
 # Use this intermediate class to customize how the icons are rendered to HTML.
 # More information available here:
-# https://github.com/tilishop/lucky_svg_sprite.cr#customizing-attributes
+# https://github.com/wout/lucky_svg_sprite#customizing-attributes
 abstract class BaseSvgIcon < LuckySvgSprite::Icon
 end
