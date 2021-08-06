3008cf713afe2afb130643250605a3a15b611a7a
Wout <hi@wout.codes>
2021-08-06 16:40:45 +0100

    fix: bug in parent class name for sprites and icons

--- templates/base_svg_sprite.cr
+++ templates/base_svg_sprite.cr
@@ -1,5 +1,5 @@
 # Use this intermediate class to customize how the sprites are rendered to HTML.
 # More information available here:
 # https://github.com/tilishop/lucky_svg_sprite.cr#customizing-attributes
-abstract class BaseSvgSprite < LuckySvgSpriteCli::Set
+abstract class BaseSvgSprite < LuckySvgSprite::Set
 end
